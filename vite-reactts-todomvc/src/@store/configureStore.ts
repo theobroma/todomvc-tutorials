6dead956a51b62c4a6e677c6192a6175ea1858a0
Alexandr Siryj <theobroma222@gmail.com>
2022-05-20 07:40:38 +0300

    integrate redux-persist

--- vite-reactts-todomvc/src/@store/configureStore.ts
+++ vite-reactts-todomvc/src/@store/configureStore.ts
@@ -1,6 +1,17 @@
 import { combineReducers, configureStore, Reducer } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { createLogger } from 'redux-logger';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { filterReducer, filterSlice } from './filter/slice';
 
@@ -18,12 +29,29 @@ export const rootReducer: Reducer<RootState> = (state, action) => {
   return combinedReducer(state, action);
 };
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  // blacklist: ['filter'], // will not be persisted
+  whitelist: ['filter'], // will be persisted
+};
+
+// Middleware: Redux Persist Persisted Reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger]),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat([logger]),
   // devTools: process.env.NODE_ENV === 'development',
   devTools: true,
 });
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof combinedReducer>;
--- vite-reactts-todomvc/src/main.tsx
+++ vite-reactts-todomvc/src/main.tsx
@@ -3,14 +3,17 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import { store } from './@store/configureStore';
+import { persistor, store } from './@store/configureStore';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={<div>...Loading</div>} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),

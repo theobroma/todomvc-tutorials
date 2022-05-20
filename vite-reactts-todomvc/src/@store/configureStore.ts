b380294fd3f85781496ae8504bb8e69317dcf999
Alexandr Siryj <theobroma222@gmail.com>
2022-05-20 04:09:48 +0300

    add redux libs

---
+++ vite-reactts-todomvc/src/@store/configureStore.ts
@@ -0,0 +1,31 @@
+import { combineReducers, configureStore, Reducer } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { createLogger } from 'redux-logger';
+
+const logger = createLogger({
+  collapsed: true,
+});
+
+const reducers = {
+  //   [uiSlice.name]: uiReducer,
+};
+
+const combinedReducer = combineReducers<typeof reducers>(reducers);
+
+export const rootReducer: Reducer<RootState> = (state, action) => {
+  return combinedReducer(state, action);
+};
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger]),
+  // devTools: process.env.NODE_ENV === 'development',
+  devTools: true,
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof combinedReducer>;
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;

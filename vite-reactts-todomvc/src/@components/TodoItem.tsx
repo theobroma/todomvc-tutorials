e3b41b4853fa04819aae8c5dc8c562d761a06a67
Alexandr Siryj <theobroma222@gmail.com>
2022-05-21 05:32:14 +0300

    start TodoItem

--- vite-reactts-todomvc/src/@components/TodoItem.tsx
+++ vite-reactts-todomvc/src/@components/TodoItem.tsx
@@ -1,10 +1,49 @@
+import { useAppDispatch } from '@store/configureStore';
+import { TodoType } from '@types';
+import clsx from 'clsx';
 import React from 'react';
 
-const TodoItem = (props) => {
-  return (
-    <div>
-      <span>TodoItem</span>
+interface Props {
+  todo: TodoType;
+  isEditing: boolean;
+}
+
+const TodoItem = ({ todo, isEditing }: Props) => {
+  const dispatch = useAppDispatch();
+
+  const element = (
+    <div className="view">
+      <input
+        className="toggle"
+        type="checkbox"
+        checked={todo.completed}
+      // onChange={() => dispatch(actions.handleTodoToggle(todo._id))}
+      />
+      <label
+        htmlFor="itself"
+      // onDoubleClick={() => dispatch(actions.editTodo(todo._id))}
+      >
+        {todo.title}
+      </label>
+      <button
+        type="button"
+        className="destroy"
+      // onClick={() => dispatch(actions.handleTodoRemove(todo._id))}
+      />
     </div>
+  );
+
+  return (
+    <li
+      className={clsx({
+        completed: todo.completed,
+        editing: isEditing,
+      })}
+    >
+      {element}
+      {/* isEditing */}
+      {/* {isEditing ? <TodoEditInput /> : null} */}
+    </li>
   );
 };
 

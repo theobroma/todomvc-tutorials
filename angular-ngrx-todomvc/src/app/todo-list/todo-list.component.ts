bef6e2fc0c7c78ec0c015430e4e1f1f7d350a9b0
Alexandr Siryj <theobroma222@gmail.com>
2022-06-01 12:21:47 +0300

    todos from store

--- angular-ngrx-todomvc/src/app/todo-list/todo-list.component.ts
+++ angular-ngrx-todomvc/src/app/todo-list/todo-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Todo } from '../modules/todos/todo.model';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
+  todos$: Observable<Todo[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private store: Store<{ todos: Todo[] }>) {
+    this.todos$ = store.select('todos');
   }
 
+  ngOnInit(): void {}
 }
--- angular-ngrx-todomvc/src/app/todo/todo.component.ts
+++ angular-ngrx-todomvc/src/app/todo/todo.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Todo } from '../modules/todos/todo.model';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent implements OnInit {
+  @Input() todo: Todo = {
+    id: 'abcdef',
+    title: 'title',
+    completed: false,
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

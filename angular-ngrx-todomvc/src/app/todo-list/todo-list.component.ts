100a4a5bdf287858c33ba2d2892f60f892e10f96
Alexandr Siryj <theobroma222@gmail.com>
2022-06-01 13:27:29 +0300

    basic todo

--- angular-ngrx-todomvc/src/app/todo-list/todo-list.component.ts
+++ angular-ngrx-todomvc/src/app/todo-list/todo-list.component.ts
@@ -1,3 +1,4 @@
+// https://stackoverflow.com/questions/39044156/how-can-i-console-log-the-value-of-a-observable
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
@@ -9,10 +10,16 @@ import { Todo } from '../modules/todos/todo.model';
   styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
-  todos$: Observable<Todo[]>;
+  todosList$: Observable<Todo[]>;
 
   constructor(private store: Store<{ todos: Todo[] }>) {
-    this.todos$ = store.select('todos');
+    const getTodos = (state: any) => state.todos;
+    const getList = (state: any) => state.list;
+
+    this.todosList$ = store.select((state) => getList(getTodos(state)));
+    this.todosList$.subscribe((res) => console.log(res));
+    // this.todos$ = store.select('todos');
+    // this.todos$.subscribe((res) => console.log(res));
   }
 
   ngOnInit(): void {}

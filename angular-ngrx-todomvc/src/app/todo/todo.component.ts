a97ac8f9ced6b34ea18584c8018023072e0ceaa7
Alexandr Siryj <theobroma222@gmail.com>
2022-06-03 16:05:27 +0300

    activeEditMode

--- angular-ngrx-todomvc/src/app/todo/todo.component.ts
+++ angular-ngrx-todomvc/src/app/todo/todo.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Todo } from '../modules/todos/todo.model';
 import { deleteTodoAC, toggleTodoAC } from '../modules/todos/todos.actions';
@@ -15,11 +15,14 @@ export class TodoComponent implements OnInit {
     title: 'title',
     completed: false,
   };
-
+  // @ViewChild('textInput') textInput: ElementRef;
+  textField: FormControl;
   checkField: FormControl;
+  editing: boolean;
 
   constructor(private store: Store<any>) {
-    // this.textField = new FormControl('', [Validators.required]);
+    this.editing = false;
+    this.textField = new FormControl('', [Validators.required]);
     this.checkField = new FormControl(false);
     this.checkField.valueChanges.subscribe((state) => {
       // console.log(state);
@@ -27,7 +30,27 @@ export class TodoComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.textField.setValue(this.todo.title);
+    this.checkField.setValue(this.todo.completed, { emitEvent: false });
+  }
+
+  updateText() {
+    console.log('updateText');
+    if (this.textField.valid) {
+      const id = this.todo.id;
+      const newText = this.textField.value;
+      // this.store.dispatch(new TodoActions.UpdateAction(id, newText));
+    }
+  }
+
+  activeEditMode() {
+    this.editing = !this.editing;
+    console.log(this.editing);
+    // setTimeout(() => {
+    //   this.textInput.nativeElement.focus();
+    // });
+  }
 
   deleteTodo() {
     const id = this.todo.id;

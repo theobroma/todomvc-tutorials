54772356c2ef4c45b49f7ae6a230a1e4e1ad0da3
Alexandr Siryj <theobroma222@gmail.com>
2022-08-23 02:05:15 +0300

    TextFieldElement

--- mui-reactts-todomvc/src/@views/MainView/MainView.tsx
+++ mui-reactts-todomvc/src/@views/MainView/MainView.tsx
@@ -1,10 +1,22 @@
-import { Container, Grid, Paper } from '@mui/material';
+import { useForm } from 'react-hook-form';
+
+import { Button, Container, Grid, Paper } from '@mui/material';
 import Box from '@mui/material/Box';
+
+import TextFieldElement from '../../@components/Forms/FormInputText';
 
 import TodoFilter from './TodoFilter';
 import TodoList from './TodoList';
 
+const defaultValues = {
+  textValue: '',
+};
+
 const MainView = () => {
+  const methods = useForm<any>({ defaultValues });
+  const { handleSubmit, reset, control, setValue, watch } = methods;
+  const onSubmit = (data: any) => console.log(data);
+
   return (
     <Container maxWidth="lg">
       <Box sx={{ flexGrow: 1 }}>
@@ -14,14 +26,30 @@ const MainView = () => {
           //  style={{ padding: 3 }}
         >
           <Grid item xs={12}>
-            <Box mb={1}>
-              <Paper elevation={3}>paper</Paper>
-            </Box>
+            <Paper elevation={3}>
+              <Box p={3}>
+                <Box
+                  style={{
+                    display: 'flex',
+                    gap: '20px',
+                  }}
+                >
+                  <TextFieldElement
+                    name="name"
+                    label="Name"
+                    control={control}
+                    fullWidth
+                  />
+                  <Button onClick={handleSubmit(onSubmit)} variant="contained">
+                    Submit
+                  </Button>
+                </Box>
+                <TodoFilter />
+                <TodoList />
+              </Box>
+            </Paper>
           </Grid>
         </Grid>
-        {/*  */}
-        <TodoFilter />
-        <TodoList />
       </Box>
     </Container>
   );

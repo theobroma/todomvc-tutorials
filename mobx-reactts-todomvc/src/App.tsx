d7e3f56e316991787689fef3aa8548f08fcd1be3
Alexandr Siryj <theobroma222@gmail.com>
2022-09-21 01:13:14 +0300

    AddTodo

--- mobx-reactts-todomvc/src/App.tsx
+++ mobx-reactts-todomvc/src/App.tsx
@@ -1,9 +1,14 @@
+import AddTodo from './components/AddTodo';
 import TodoList from './components/TodoList';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">react</header>
+      <header className="App-header">
+        <h1>MobX TodoMVC</h1>
+      </header>
+      <AddTodo />
+      <br />
       <TodoList />
     </div>
   );
---
+++ mobx-reactts-todomvc/src/components/AddTodo.tsx
@@ -0,0 +1,55 @@
+import React, { useContext, useState } from 'react';
+import { observer } from 'mobx-react';
+
+import TodoStore from '../stores/TodoStore';
+
+const AddTodo = () => {
+  const [title, setTitle] = useState('');
+  const todoStore = useContext(TodoStore);
+  const { addTodo, info } = todoStore;
+
+  return (
+    <>
+      <div className="alert alert-primary">
+        <div className="d-inline col-4">
+          Total items: &nbsp;
+          <span className="badge badge-info">{info.total}</span>
+        </div>
+        <div className="d-inline col-4">
+          Finished items: &nbsp;
+          <span className="badge badge-info">{info.completed}</span>
+        </div>
+        <div className="d-inline col-4">
+          Unfinished items: &nbsp;
+          <span className="badge badge-info">{info.notCompleted}</span>
+        </div>
+      </div>
+      <div className="form-group">
+        <input
+          className="form-control"
+          type="text"
+          value={title}
+          placeholder="Todo title..."
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={(_) => {
+            addTodo({
+              title,
+              completed: false,
+            });
+            setTitle('');
+          }}
+        >
+          Add Todo
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default observer(AddTodo);

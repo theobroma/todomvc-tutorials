48bdf8a9d9af7faeaca22ccee32ce58e7032cfd6
Alexandr Siryj <theobroma222@gmail.com>
2022-11-01 05:00:19 +0200

    items

--- msw-grocery-list/src/App.tsx
+++ msw-grocery-list/src/App.tsx
@@ -1,13 +1,62 @@
-import MainView from './@views/MainView';
+import useSWR from 'swr';
 
-// className="App" for testing tw linter
-const App = () => {
+import { CreateItem } from './@components/CreateItem';
+import { Item } from './@components/Item';
+
+const itemsEndpoint = 'http://localhost:3001/grocery-list';
+
+const getItem = async () => {
+  const response = await fetch(`${itemsEndpoint}/`);
+  return response.json();
+};
+
+const ItemApp = () => {
+  const key = `${itemsEndpoint}`;
+
+  const { data: items, mutate } = useSWR(key, () => getItem());
+  console.log('items', items);
+
+  const loading = !items;
+
   return (
-    <div className="App">
-      <h1 className="text-center text-3xl font-bold underline">MSW</h1>
-      <MainView />
+    <div className="mx-auto max-w-lg pt-4">
+      <h1 className="pb-4 text-4xl font-semibold">Grocery List</h1>
+      <CreateItem
+        disabled={loading}
+        mutate={mutate}
+        onCreate={(newItem: any) => {
+          mutate([...items, newItem]);
+        }}
+      />
+      {!!items && items.length > 0 && (
+        <ul>
+          {items.map((item: any) => {
+            return (
+              <Item
+                item={item}
+                key={item.id}
+                onUpdate={(newItem: any) => {
+                  mutate(
+                    items.map((listItem: any) => {
+                      if (item.id !== listItem.id) {
+                        return item;
+                      }
+                      return newItem;
+                    }),
+                  );
+                }}
+                onDelete={() => {
+                  mutate(
+                    items.filter((listItem: any) => listItem.id !== item.id),
+                  );
+                }}
+              />
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
 
-export default App;
+export default ItemApp;

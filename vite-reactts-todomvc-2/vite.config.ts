55349ed1a5031fc36f3b4909e2c1aef4ebeb3c28
Alexandr Siryj <theobroma222@gmail.com>
2023-03-07 07:39:07 +0200

    @types/node

--- vite-reactts-todomvc-2/vite.config.ts
+++ vite-reactts-todomvc-2/vite.config.ts
@@ -1,5 +1,7 @@
+// https://stackoverflow.com/questions/75201705/how-to-set-multiple-aliases-in-vite-react
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({

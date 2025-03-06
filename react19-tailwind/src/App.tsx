648fc82ed9458b11369e6c7fba63daad976fe6d1
Alexandr Siryj <theobroma222@gmail.com>
2025-03-06 21:05:26 +0200

    routes

--- react19-tailwind/src/App.tsx
+++ react19-tailwind/src/App.tsx
@@ -1,8 +1,16 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { UsersPage } from './pages/users';
+import { TodoListPage } from './pages/todo-list';
+
 function App() {
     return (
-        <>
+        <BrowserRouter>
             <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        </>
+            <Routes>
+                <Route path="/" element={<UsersPage />}></Route>
+                <Route path="/:userId/tasks" element={<TodoListPage />}></Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 

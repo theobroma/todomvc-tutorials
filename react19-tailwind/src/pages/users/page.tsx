75cf3ff369039a12913921af9e10afb5aabc45d2
Alexandr Siryj <theobroma222@gmail.com>
2025-03-08 17:19:25 +0200

    del users

--- react19-tailwind/src/pages/users/page.tsx
+++ react19-tailwind/src/pages/users/page.tsx
@@ -1,9 +1,9 @@
 import { Suspense, useActionState, useOptimistic, useRef } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { Link } from 'react-router-dom';
-import { noop } from '../../utils/noop.util';
+// import { noop } from '../../utils/noop.util';
 import { User } from '../../shared/api';
-import { CreateUserAction } from './actions';
+import { CreateUserAction, DeleteUserAction } from './actions';
 import { useUsers } from './use-users';
 
 export function CreateUserForm({
@@ -47,45 +47,46 @@ export function CreateUserForm({
     );
 }
 
-export const UserCard = ({
+export function UserCard({
     user,
     deleteUserAction,
 }: {
     user: User;
-    deleteUserAction: any;
-}) => (
-    <div className="border p-2 m-2 rounded bg-gray-100 flex gap-2">
-        {user.email}
+    deleteUserAction: DeleteUserAction;
+}) {
+    const [state, handleDelete] = useActionState(deleteUserAction, {});
 
-        <form className="ml-auto">
-            <input type="hidden" name="id" value={user.id} />
-            <Link
-                to={`/${user.id}/tasks`}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto disabled:bg-gray-400"
-            >
-                Tasks
-            </Link>
-            <button
-                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-auto disabled:bg-gray-400"
-                formAction={deleteUserAction}
-            >
-                Delete
-                {/* {state.error && (
-                <div className="text-red-500">
-                    {state.error}
-                </div>
-            )} */}
-            </button>
-        </form>
-    </div>
-);
+    return (
+        <div className="border p-2 m-2 rounded bg-gray-100 flex gap-2">
+            {user.email}
 
+            <form className="ml-auto">
+                <input type="hidden" name="id" value={user.id} />
+                <Link
+                    to={`/${user.id}/tasks`}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto disabled:bg-gray-400"
+                >
+                    Tasks
+                </Link>
+                <button
+                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-auto disabled:bg-gray-400"
+                    formAction={handleDelete}
+                >
+                    Delete
+                    {state.error && (
+                        <div className="text-red-500">{state.error}</div>
+                    )}
+                </button>
+            </form>
+        </div>
+    );
+}
 export function UsersList({
     deleteUserAction,
     useUsersList,
 }: {
     useUsersList: () => User[];
-    deleteUserAction: any;
+    deleteUserAction: DeleteUserAction;
 }) {
     const users = useUsersList();
     return (
@@ -116,19 +117,10 @@ export const UsersPage = () => {
             >
                 <Suspense fallback={<div>Loading...</div>}>
                     <UsersList
-                        // deleteUserAction={deleteUserAction}
-                        // useUsersList={useUsersList}
-                        deleteUserAction={noop}
-                        useUsersList={() => [
-                            {
-                                id: '1',
-                                email: 'example@gmail.com',
-                            },
-                            {
-                                id: '2',
-                                email: 'dfgdfgdfg@gmail.com',
-                            },
-                        ]}
+                        deleteUserAction={deleteUserAction}
+                        useUsersList={useUsersList}
+                        // deleteUserAction={noop}
+                        // useUsersList={() => []}
                     />
                 </Suspense>
             </ErrorBoundary>

93b4ca7c0217f66bdf99bb217e93c72aeba8c628
Alexandr Siryj <theobroma222@gmail.com>
2025-04-05 15:36:54 +0300

    config

--- vite-r19-linaria/vite.config.ts
+++ vite-r19-linaria/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import wyw from '@wyw-in-js/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        wyw({
+            include: ['**/*.{ts,tsx}'],
+            babelOptions: {
+                presets: ['@babel/preset-typescript', '@babel/preset-react'],
+            },
+        }),
+        react(),
+    ],
+});

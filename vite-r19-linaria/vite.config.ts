de5f1760016aae8afd131c0777734c86cd00c94d
Alexandr Siryj <theobroma222@gmail.com>
2025-04-05 15:46:36 +0300

    format

--- vite-r19-linaria/vite.config.ts
+++ vite-r19-linaria/vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import wyw from '@wyw-in-js/vite';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        wyw({
-            include: ['**/*.{ts,tsx}'],
-            babelOptions: {
-                presets: ['@babel/preset-typescript', '@babel/preset-react'],
-            },
-        }),
-        react(),
-    ],
+  plugins: [
+    wyw({
+      include: ['**/*.{ts,tsx}'],
+      babelOptions: {
+        presets: ['@babel/preset-typescript', '@babel/preset-react'],
+      },
+    }),
+    react(),
+  ],
 });

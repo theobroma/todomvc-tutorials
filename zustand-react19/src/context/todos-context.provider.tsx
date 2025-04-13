1f4a2c81018cfe7f98a20c5877ffc71bff16d550
Alexandr Siryj <theobroma222@gmail.com>
2025-04-13 21:53:07 +0300

    init zustand-react19

---
+++ zustand-react19/src/context/todos-context.provider.tsx
@@ -0,0 +1,90 @@
+import { PropsWithChildren, useMemo, useState } from 'react';
+
+import { FilterEnum } from '@/enums/filter.enum';
+
+import { TodosContext } from './todos-context';
+import { initialTodos } from './todos-context.initial';
+import { TodoInterface } from './todos-context.interface';
+
+export const TodosContextProvider = ({ children }: PropsWithChildren) => {
+  const [todos, setTodos] = useState<TodoInterface[]>(initialTodos);
+  const [filter, setFilter] = useState<FilterEnum>(FilterEnum.ShowAll);
+  const [editingTodoId, setEditingTodoId] = useState<null | string>(null);
+
+  const activeTodoCount = useMemo(
+    () => todos?.reduce((acc, todo) => acc + (todo.completed ? 0 : 1), 0),
+    [todos],
+  );
+
+  const completedTodoCount = useMemo(
+    () => todos?.reduce((acc, todo) => acc + (todo.completed ? 1 : 0), 0),
+    [todos],
+  );
+
+  const addTodo = (title: string) => {
+    const newTodo = {
+      id: crypto.randomUUID(),
+      title,
+      completed: false,
+    };
+
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleTodo = (id: string) => {
+    const newTodos = todos.map((todo) => ({
+      ...todo,
+      completed: todo.id === id ? !todo.completed : todo.completed,
+    }));
+
+    setTodos(newTodos);
+  };
+
+  const toggleTodos = () => {
+    const isAllCompleted = todos.length === completedTodoCount;
+
+    const newTodos = todos.map((todo) => ({
+      ...todo,
+      completed: !isAllCompleted,
+    }));
+
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (id: string) => {
+    const filteredTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(filteredTodos);
+  };
+
+  const removeCompleted = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  };
+
+  const changeFilter = (filter: FilterEnum) => setFilter(filter);
+
+  const editTodo = (id: string) => {
+    setEditingTodoId(id);
+  };
+
+  return (
+    <TodosContext.Provider
+      value={{
+        todos,
+        activeTodoCount,
+        completedTodoCount,
+        filter,
+        editingTodoId,
+        addTodo,
+        toggleTodo,
+        toggleTodos,
+        deleteTodo,
+        editTodo,
+        removeCompleted,
+        changeFilter,
+      }}
+    >
+      {children}
+    </TodosContext.Provider>
+  );
+};
---
+++ zustand-react19/src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import 'todomvc-app-css/index.css';
+
+import App from './App.tsx';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);

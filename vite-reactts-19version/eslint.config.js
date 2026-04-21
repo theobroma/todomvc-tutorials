81bbea932cde77db923d2a74bb50e53ed1cbfc0f
Alexandr Siryj <theobroma222@gmail.com>
2026-04-21 21:20:42 +0300

    add prettier to eslint config

--- vite-reactts-19version/eslint.config.js
+++ vite-reactts-19version/eslint.config.js
@@ -1,4 +1,6 @@
 import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
@@ -7,7 +9,11 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettierConfig, // Must be last to override other configs
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -16,9 +22,12 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      // prettier-ignore
+      'prettier': prettierPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'prettier/prettier': 'error', // Report prettier issues as ESLint errors
       'no-console': 'warn', // Забороняємо використання всіх console.*
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       '@typescript-eslint/no-unused-vars': [

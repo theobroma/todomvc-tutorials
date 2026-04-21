8c05216b9954d0f339f8ca03cab446d6048ce24f
Alexandr Siryj <theobroma222@gmail.com>
2026-04-21 22:03:46 +0300

    path aliases

--- vite-reactts-19version/vite.config.ts
+++ vite-reactts-19version/vite.config.ts
@@ -1,7 +1,17 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [tsconfigPaths(), react()],
+    base: '/',
+  };
+
+  if (command !== 'serve') {
+    config.base = '/todomvc-tutorials/';
+  }
+
+  return config;
 });

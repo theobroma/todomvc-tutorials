a26393590d0e0842b1eadabe03c612528128e49f
Alexandr Siryj <theobroma222@gmail.com>
2026-04-22 13:21:32 +0300

    react compiler

--- vite-reactts-19version/vite.config.ts
+++ vite-reactts-19version/vite.config.ts
@@ -1,11 +1,18 @@
-import react from '@vitejs/plugin-react-swc';
+import babel from '@rolldown/plugin-babel';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const config = {
-    plugins: [tsconfigPaths(), react()],
+    plugins: [
+      tsconfigPaths(),
+      react(),
+      babel({
+        presets: [reactCompilerPreset()],
+      }),
+    ],
     base: '/',
   };
 

60391cfd01c29f471e33955ccec10059d90293ac
Alexandr Siryj <theobroma222@gmail.com>
2026-04-23 15:58:19 +0300

    react-hooks rules

--- vite-reactts-19version/eslint.config.js
+++ vite-reactts-19version/eslint.config.js
@@ -31,8 +31,18 @@ export default tseslint.config(
       ...reactHooks.configs.recommended.rules,
       'prettier/prettier': 'error', // Report prettier issues as ESLint errors
       'no-console': 'warn', // Забороняємо використання всіх console.*
+
+      'react-hooks/todo': 'error',
+      // другие полезные правила:
+      'react-hooks/capitalized-calls': 'error', // не вызывайте функции с заглавной буквы (нужно использовать JSX)
+      'react-hooks/hooks': 'error', // во многом пере-реализует некомпиляторное правило «rules-of-hooks»
+      'react-hooks/rule-suppression': 'error', // проверяет корректность подавления других правил
+      'react-hooks/syntax': 'error', // проверяет некорректный синтаксис
+      'react-hooks/unsupported-syntax': 'error', // по умолчанию `warn`, используйте `error`, чтобы ронять сборку
+
       'react-compiler/react-compiler': 'error', // Включаємо правило для React Compiler
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
       '@typescript-eslint/no-unused-vars': [
         'error',
         { ignoreRestSiblings: true, argsIgnorePattern: '^_' },
